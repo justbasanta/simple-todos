3c4e93876c1b27159fabb491105fb54ddcaa5328
Basanta Thapa <justbasanta5@gmail.com>
2016-10-16 20:13:45 +0545

    forms and events

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,11 +1,34 @@
 import { Template } from 'meteor/templating';
- 
+ import { Tasks } from '../api/tasks.js';
+
 import './body.html';
  
 Template.body.helpers({
-  tasks: [
-    { text: 'This is task 1' },
-    { text: 'This is task 2' },
-    { text: 'This is task 3' },
-  ],
+  tasks(){
+  	return Tasks.find({}, {sort: { createdAt: -1}});
+  },
+});
+
+Template.body.events({
+	'submit .new-task'(event){
+		//prevent default browser from submit
+		event.preventDefault();
+
+		//Get value from form element
+
+	const target = event.target;
+	const text = target.text.value;
+	console.log(event);
+
+	//Insert a task into collection
+	Tasks.insert({
+		text,
+		createdAt: new Date(), //current time
+
+	});
+	//clear form
+
+	target.text.value = '';
+	//return false;
+	},
 });
